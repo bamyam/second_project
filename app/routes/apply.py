68d2a990343ee3998c924d74b93f2c2acb0eaf02
SEONGEUN, KIM <manuma159159@gmail.com>
2024-04-19 18:24:12 +0900

    방문신청 정리(미완)

--- app/routes/apply.py
+++ app/routes/apply.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter
 from fastapi.responses import HTMLResponse
 from fastapi.requests import Request
 from fastapi.templating import Jinja2Templates
+from app.schemas.visitors import Visitors
+from app.services.visitors import VisitorsService
 
 apply_router = APIRouter()
 
@@ -9,6 +11,14 @@ templates = Jinja2Templates(directory='views/templates')
 
 
 @apply_router.get('/', response_class=HTMLResponse)
-def check(req: Request):
+def apply(req: Request):
     return templates.TemplateResponse('apply/apply.html', {'request': req})
 
+@apply_router.post('/apply')
+def apply(vmdto: Visitors):
+    result = VisitorsService.insert_visitor(vmdto)
+    return result.rowcount
+
+@apply_router.get('/apply', response_class=HTMLResponse)
+def applyok(req:Request):
+    return templates.TemplateResponse('/applyok.html', {'request': req})
