395af1b2f10ef2ca1fc5f7cfe1239561371c4fe6
Yunsung Lim <kwngjin@gmail.com>
2024-04-23 11:07:49 +0900

    검색 관련 staticmethod 추가

--- app/services/visitors.py
+++ app/services/visitors.py
@@ -1,5 +1,5 @@
 from app.models.visitors import Visitors
-from sqlalchemy import insert
+from sqlalchemy import insert, select, and_
 from app.dbfactory import Session
 
 
@@ -39,3 +39,12 @@ class VisitorsService:
         }
 
         return data
+
+    @staticmethod
+    def search_visitor(name, phone_number):
+        with Session() as sess:
+            # name과 phone_number를 모두 만족하는 방문객 정보 조회
+            stmt = select(Visitors).where(and_(Visitors.name == name, Visitors.phone_number == phone_number))
+            result = sess.execute(stmt).fetchall()
+
+        return result
