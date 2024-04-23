edca3a20a352365046c89b72cfa5a11c5ee070c9
Seungjae, Seo <seoseungjae20209@gmail.com>
2024-04-23 11:27:23 +0900

    방문수락, 거절 기능, 메일 발송 기능

--- app/routes/admin.py
+++ app/routes/admin.py
@@ -15,28 +15,36 @@ templates = Jinja2Templates(directory='views/templates')
 
 @admin_router.get('/', response_class=HTMLResponse)
 def admin(req: Request):
-    return templates.TemplateResponse('admin/admin.html', {'request': req})
+    info = AdminService.select_visit()
+    return templates.TemplateResponse('admin/admin.html', {'request': req,
+        'info': info})
 
 
 @admin_router.post('/accept')
 def accept(data: dict):
     number = data['number']
+
     result = AdminService.accept_visit(number)
+
     res_url = '/error'
     if result.rowcount > 0:
         res_url = '/admin'
         visitor_name, visitor_email, time = MailService.find_data(number)
         MailService.mail_accepted(visitor_name, visitor_email, time)
+
     return RedirectResponse(res_url, status_code=status.HTTP_302_FOUND)
 
 @admin_router.post('/reject')
 def reject(data: dict):
     number = data['number']
     reason = data['reason']
+
     result = AdminService.reject_visit(number)
+
     res_url = '/error'
     if result.rowcount > 0:
         res_url = '/admin'
         visitor_name, visitor_email, time = MailService.find_data(number)
         MailService.mail_regected(visitor_name, visitor_email, time, reason)
+
     return RedirectResponse(res_url, status_code=status.HTTP_302_FOUND)
--- app/services/admin.py
+++ app/services/admin.py
@@ -1,9 +1,21 @@
-from sqlalchemy import update
+from sqlalchemy import update, select
 
 from app.dbfactory import Session
-from app.models.visitors import Visitors
+from app.models.visitors import Visitors, Employee, Location
 
 class AdminService:
+    @staticmethod
+    def select_visit():
+        with Session() as sess:
+            stmt = select(Visitors.id, Visitors.regdate, Visitors.company_name, Visitors.department_name, Visitors.job_position,
+                          Visitors.name, Visitors.phone_number, Employee.department_name, Employee.job_position, Employee.name,
+                          Employee.security_grade, Visitors.purpose, Location.location, Location.security_grade, Visitors.visit_date, Visitors.status)\
+            .join_from(Visitors, Employee)\
+            .join_from(Visitors, Location)
+            result = sess.execute(stmt)
+
+        return result
+
     @staticmethod
     def accept_visit(number):
         with Session() as sess:
@@ -14,8 +26,7 @@ class AdminService:
         return result
 
     @staticmethod
-    def reject_visit(data):
-        number = data['number']
+    def reject_visit(number):
         with Session() as sess:
             stmt = update(Visitors).where(Visitors.id == number).values(status='R')
             result = sess.execute(stmt)
