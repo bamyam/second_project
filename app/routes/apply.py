308cf4860885d9f253266fa0ee598d3b247fcc63
Yunsung Lim <kwngjin@gmail.com>
2024-04-23 22:38:35 +0900

    location 정보 추가

--- app/routes/apply.py
+++ app/routes/apply.py
@@ -5,7 +5,8 @@ from fastapi.templating import Jinja2Templates
 from app.schemas.visitors import Visitors
 from app.services.visitors import VisitorsService
 from app.services.employee import EmployeeService
-
+from app.services.location import LocationService
+from fastapi import HTTPException
 
 apply_router = APIRouter()
 
@@ -15,12 +16,14 @@ templates = Jinja2Templates(directory='views/templates')
 @apply_router.get('/', response_class=HTMLResponse)
 def apply(req: Request):
     emp_list = EmployeeService.get_all_employees()
-    return templates.TemplateResponse('apply/apply.html', {'request': req, 'emp_list': emp_list})
+    location_list = LocationService.get_all_location()
+    return templates.TemplateResponse('apply/apply.html', {'request': req, 'emp_list': emp_list, 'location_list': location_list})
 
 
 
 @apply_router.post('/applyok')
 def applyok(vmdto: Visitors):
+    print(vmdto)
     result = VisitorsService.insert_visitor(vmdto)
     return result.rowcount
 
@@ -28,3 +31,5 @@ def applyok(vmdto: Visitors):
 @apply_router.get('/applyok', response_class=HTMLResponse)
 def applycheck(req: Request):
     return templates.TemplateResponse('apply/applyok.html', {'request': req})
+
+
